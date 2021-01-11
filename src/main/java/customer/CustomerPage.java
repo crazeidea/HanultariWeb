8eba976bf84952df5a6c09f99312d373214a943a
DESKTOP-RMS29GL\user <songjae0708@gmail.com>
2021-01-11 09:38:46 +0900

    no message

--- src/main/java/customer/CustomerPage.java
+++ src/main/java/customer/CustomerPage.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
-import command.PageDTO;
+import common.PageDTO;
 
 @Component
 public class CustomerPage extends PageDTO{
